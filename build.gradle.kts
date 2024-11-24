4994b46c2bb2ff1056093bc16b0487429728ff66
Dan <dan.chadfield@gmail.com>
2024-11-24 17:38:47 +0000

    Add logback configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,6 +18,8 @@ dependencies {
     testImplementation(kotlin("test"))
     implementation("io.ktor:ktor-server-core:2.1.2")
     implementation("io.ktor:ktor-server-netty:2.1.2")
+    implementation("ch.qos.logback:logback-classic:1.4.4")
+    implementation("org.slf4j:slf4j-api:2.0.3")
 }
 
 tasks.test {
