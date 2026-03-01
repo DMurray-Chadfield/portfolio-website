4bcc0a1fc4fcbcfc8a991a2bc34c0c9667dc8d07
Dan Murray-Chadfield <dan.chadfield@gmail.com>
2026-03-01 18:58:36 +0000

    Fix coroutine test by moving internal server into BootstrapWebApp

--- src/main/kotlin/kotlinbook/BootstrapWebApp.kt
+++ src/main/kotlin/kotlinbook/BootstrapWebApp.kt
@@ -1,11 +1,22 @@
 package kotlinbook
 
 import io.ktor.server.application.ApplicationStarting
+import io.ktor.server.application.call
 import io.ktor.server.engine.BaseApplicationResponse
 import io.ktor.server.engine.applicationEngineEnvironment
 import io.ktor.server.engine.defaultEnginePipeline
+import io.ktor.server.engine.embeddedServer
 import io.ktor.server.engine.installDefaultTransformations
+import io.ktor.server.netty.Netty
+import io.ktor.server.request.receiveText
+import io.ktor.server.response.respondText
+import io.ktor.server.routing.get
+import io.ktor.server.routing.post
+import io.ktor.server.routing.routing
 import io.ktor.server.servlet.ServletApplicationEngine
+import kotlinbook.web.response.TextWebResponse
+import kotlinbook.web.webResponse
+import kotlinx.coroutines.delay
 import kotlinbook.db.datasource.createAndMigrateDataSource
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.support.DefaultListableBeanFactory
@@ -29,6 +40,32 @@ class BootstrapWebApp : ServletContextListener {
         ) as WebappConfig
         log.debug("Setting up data source")
         val dataSource = createAndMigrateDataSource(appConfig)
+
+        log.debug("Starting internal server on port 9876")
+        val internalServer = embeddedServer(Netty, port = 9876) {
+            routing {
+                get("/random_number", webResponse {
+                    val num = (200L..2000L).random()
+                    delay(num)
+                    TextWebResponse(num.toString())
+                })
+
+                get("/ping", webResponse {
+                    TextWebResponse("pong")
+                })
+
+                post("/reverse", webResponse {
+                    TextWebResponse(call.receiveText().reversed())
+                })
+            }
+        }
+        try {
+            internalServer.start(wait = false)
+            log.debug("Internal server started successfully on port 9876")
+        } catch (e: Exception) {
+            log.error("Failed to start internal server on port 9876", e)
+            throw e
+        }
 
         log.debug("Setting up Ktor servlet environment")
         val appEngineEnvironment = applicationEngineEnvironment {
